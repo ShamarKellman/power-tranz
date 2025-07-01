a9bb4fa8392990f35e1e684aac8e7693b327bb26
ShamarKellman <4120411+ShamarKellman@users.noreply.github.com>
2025-07-01 22:30:22 +0000

    Fix styling

--- src/PowerTranz.php
+++ src/PowerTranz.php
@@ -467,7 +467,7 @@ class PowerTranz implements PowerTranzInterface
 
     private function timestamp(): string
     {
-        $now = new DateTimeImmutable;
+        $now = new DateTimeImmutable();
 
         return $now->format('YmdHisu');
     }
